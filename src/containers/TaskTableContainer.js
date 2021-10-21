3e9f4cd1c2056c561f55f822baeb9fc7119ceb0c
Yumapon <keropon1274@gmail.com>
2021-10-21 23:12:48 +0900

    ciを追加

--- src/containers/TaskTableContainer.js
+++ src/containers/TaskTableContainer.js
@@ -2,7 +2,7 @@ import React,{ useEffect } from "react";
 import HomePageTaskData from "../components/organisms/HomePageTaskDataOrganisms";
 import { useSelector, useDispatch} from "react-redux";
 import { selectTasks, fetchTasks, seletTasksCleared } from "../modules/DBTasks";
-import { selectTask, taskCleared, taskNameEdited, taskContentsEdited, deadlineEdited, clientEdited, createTask, updateTask, deleteTask } from "../modules/Task";
+import { taskNameEdited, deadlineEdited, clientEdited, createTask, updateTask, deleteTask } from "../modules/Task";
 
 
 const TaskTableContainer = () => {
@@ -59,31 +59,6 @@ const TaskTableContainer = () => {
           data={JSON.parse(JSON.stringify(tasks))} 
         />
     )
-
-    //JSON.parse(JSON.stringify(useSelector(state=>state.cycleSlice))); 
-    
-    /*
-    const rows = [
-      { id: 1, name: '丘本悠真', content: 'testetstテスト', deadline: '2005-06-07', client: '田中慎吾'},
-      { id: 2, name: 'test2', content: 'testetst2', deadline: '2021-09-19', client: 'test2'},
-      { id: 3, deadline: 'Lannister', name: 'Jaime', client: 45 },
-      { id: 4, deadline: 'Stark', name: 'Arya', client: 16 },
-      { id: 5, deadline: 'Targaryen', name: 'Daenerys', client: null },
-      { id: 6, deadline: 'Melisandre', name: null, client: 150 },
-      { id: 7, deadline: 'Clifford', name: 'Ferrara', client: 44 },
-      { id: 8, deadline: 'Frances', name: 'Rossini', client: 36 },
-      { id: 9, deadline: 'Roxie', name: 'Harvey', client: 65 },
-    ];
-
-   return(
-    <HomePageTaskData 
-      rowAddHandler={null}
-      rowUpdateHandler={null}
-      rowDeleteHandler={null}
-      data={rows} 
-    />
-   )
-   */
 };
 
 export default TaskTableContainer;
