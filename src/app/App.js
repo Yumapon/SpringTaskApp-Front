3264033d318d7a6b15d4d5d529773e7382234ab6
丘本悠真 <yuma@okamotoyuumanoMac-mini.local>
2022-02-19 09:30:30 +0900

    a

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import Toppage from "../pages/TopPage2";
+import LoginPage from "../pages/LoginPage";
+import UserInfo from "../pages/UserInfoPage";
 
 function App() {
   return (
